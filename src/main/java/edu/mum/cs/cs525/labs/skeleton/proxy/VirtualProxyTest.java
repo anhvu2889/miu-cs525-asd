c0e6836784b51b6447461725dde17cec8cdd7bf8
Huynh Anh Vu Nguyen (Cris) <huynhanhvu.nguyen@miu.edu>
2024-09-18 20:51:06 -0500

    Fix Lab 9 - Proxy Only instantiate Complex Object when invoking method

--- src/main/java/edu/mum/cs/cs525/labs/skeleton/proxy/VirtualProxyTest.java
+++ src/main/java/edu/mum/cs/cs525/labs/skeleton/proxy/VirtualProxyTest.java
@@ -3,8 +3,12 @@ package edu.mum.cs.cs525.labs.skeleton.proxy;
 
 public class VirtualProxyTest {
     public static void main(String[] args) throws InterruptedException {
+        Complex complexButDoNothing = VirtualProxy.getInstance();
+        Thread.sleep(5000);
+        System.out.println("Trying to create another proxy");
         Complex complexThing = VirtualProxy.getInstance();
         complexThing.veryComplicatedTask();
         complexThing.veryComplicatedTask();
+
     }
 }
